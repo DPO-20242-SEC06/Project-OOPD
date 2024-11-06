2388f71902213c487bb1ff03995ef6fe5dd56d30
jebriceno2 <157536964+jebriceno2@users.noreply.github.com>
2024-11-06 14:12:49 -0500

    Update UserProfileManagement.java

--- app/src/user_role_management/user_management/UserProfileManagement.java
+++ app/src/user_role_management/user_management/UserProfileManagement.java
@@ -1,5 +1,18 @@
 package user_role_management.user_management;
 
 public class UserProfileManagement {
+    private PreferenceManager preferenceManager;
 
+    public User getUserProfile(String username) {
+
+        return null;
+    }
+
+    public void updateUserProfile(String username, String newName, String newEmail) {
+
+    }
+
+    public void createUser(String username, String name, String email, String password) {
+
+    }
 }
