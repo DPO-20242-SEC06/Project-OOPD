3342a6c303c18c5dad4ba2994688b9c4903fe876
Simoncl20 <simoncalderonl2010@gmail.com>
2024-11-06 02:23:44 -0500

    Implement Exam and Quiz classes with question management and grading functionality

--- app/src/lp_act_mg/activity/Exam.java
+++ app/src/lp_act_mg/activity/Exam.java
@@ -1,5 +1,42 @@
 package lp_act_mg.activity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Exam {
+
+    public final String EXAM = "Exam";
+    private int grade;
+    private String statusSubmission;
+    private List<ExamQuestion> questions;
+
+    public Exam(String statusSubmission) {
+        this.statusSubmission = statusSubmission;
+        this.questions = new ArrayList<>(); 
+    }
+
+    public void addQuestion(ExamQuestion question) {
+        questions.add(question);
+    }
+
+    public List<ExamQuestion> getQuestions() {
+        return questions;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public String getStatusSubmission() {
+        return statusSubmission;
+    }
+
+    public void setStatusSubmission(String statusSubmission) {
+        this.statusSubmission = statusSubmission;
+    }
 
 }
--- app/src/lp_act_mg/activity/Quiz.java
+++ app/src/lp_act_mg/activity/Quiz.java
@@ -1,5 +1,45 @@
 package lp_act_mg.activity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Quiz {
+
+    public final String QUIZ = "Quiz";
+    private int approvalGrade;
+    private List<QuizQuestion> questions;
+
+
+    public Quiz(int approvalGrade) {
+        this.approvalGrade = approvalGrade;
+        this.questions = new ArrayList<>();
+    }
+
+    public void addQuestion(QuizQuestion question) {
+        questions.add(question);
+    }
+
+    public int getApprovalGrade() {
+        return approvalGrade;
+    }
+
+    public List<QuizQuestion> getQuestions() {
+        return questions;
+    }
+
+    public boolean isApproved(int grade) {
+        return grade >= approvalGrade;
+    }
+
+    public int getGrade(List<Option> answers) {
+        int grade = 0;
+        for (int i = 0; i < questions.size(); i++) {
+            if (questions.get(i).isCorrect(answers.get(i))) {
+                grade++;
+            }
+        }
+        return grade;
+    }
+
 
 }
