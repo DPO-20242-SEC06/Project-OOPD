194039c919b6601c7ae797b6b15ff9da7345da0b
jebriceno2 <157536964+jebriceno2@users.noreply.github.com>
2024-11-06 14:12:24 -0500

    Update User.java

--- app/src/user_role_management/user_management/User.java
+++ app/src/user_role_management/user_management/User.java
@@ -1,5 +1,20 @@
 package user_role_management.user_management;
 
 public abstract class User {
+    protected String username;
+    protected String password;
+    protected String name;
+    protected String email;
+
+    public User(String username, String password, String name, String email) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public boolean login(String username, String password) {
+        return this.username.equals(username) && this.password.equals(password);
+    }
 
 }
