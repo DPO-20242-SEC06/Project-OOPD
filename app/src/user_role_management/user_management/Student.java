387c076e77ee5770fb136ed1e57a94690d53c282
jebriceno2 <157536964+jebriceno2@users.noreply.github.com>
2024-11-06 14:12:01 -0500

    Update Student.java

--- app/src/user_role_management/user_management/Student.java
+++ app/src/user_role_management/user_management/Student.java
@@ -1,5 +1,26 @@
 package user_role_management.user_management;
 
 public class Student extends User {
+    public static final String STUDENT = "Student";
+    private String preferences;
+    private List<LearningProgress> currentProgresses;
+    private List<LearningProgress> completed;
+    private List<Attempt> attempts;
 
+    public Student(String username, String password, String name, String email) {
+        super(username, password, name, email);
+    }
+
+    public void enrollLearningPath(LearningPath lp) {
+        currentProgresses.add(new LearningProgress(lp));
+    }
+
+    public void completeActivity(Activity activity) {
+        // lógica para completar una actividad
+    }
+
+    public LearningProgress viewLearningProgress(LearningPath lp) {
+        // lógica para ver el progreso de aprendizaje
+        return null;
+    }
 }
