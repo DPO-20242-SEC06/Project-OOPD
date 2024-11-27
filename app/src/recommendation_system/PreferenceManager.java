1b75973d5588b787a5f62f2a34779058885ea050
JhonCastro-023 <157712811+JhonCastro-023@users.noreply.github.com>
2024-11-27 12:24:09 -0500

    :)

--- app/src/recommendation_system/PreferenceManager.java
+++ app/src/recommendation_system/PreferenceManager.java
@@ -1,5 +1,40 @@
 package recommendation_system;
 
+import java.util.List;
+import java.util.Map;
+
+import lp_act_mg.activity.Activity;
+import lp_act_mg.learning_path.LearningPath;
+import user_role_management.user_management.Student;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class PreferenceManager {
+    private Map<String, List<String>> studentPreferences;
+
+    public PreferenceManager() {
+        this.studentPreferences = new HashMap<>();
+    }
+
+    public List<String> getStudentPreferences(String studentUser) {
+        return studentPreferences.getOrDefault(studentUser, new ArrayList<>());
+    }
+
+    public List<Activity> recommendNextActivity(Student student, LearningPath lp) {
+        List<Activity> activities = new ArrayList<>();
+        List<Activity> learningPathActivities = lp.getActivities(); 
+        List<String> preferences = getStudentPreferences(student.getUsername());
+
+        for (Activity activity : learningPathActivities) {
+            if (preferences.contains(activity.getRating())) { 
+                activities.add(activity);
+                break; 
+            }
+        }
+
+        return activities;
+    }
+
 
 }
--- app/src/recommendation_system/RecommendationService.java
+++ app/src/recommendation_system/RecommendationService.java
@@ -1,5 +1,32 @@
 package recommendation_system;
 
-public class RecommendationService {
+import java.util.List;
 
+import lp_act_mg.activity.Activity;
+import lp_act_mg.learning_path.LearningPath;
+import user_role_management.user_management.Student;
+
+import java.util.ArrayList;
+
+public class RecommendationService {
+    private PreferenceManager preferenceManager;
+
+    public RecommendationService() {
+        this.preferenceManager = new PreferenceManager();
+    }
+
+    public List<LearningPath> recommendLearningPath(Student student) {
+        List<String> preferences = preferenceManager.getStudentPreferences(student.getUsername());
+        List<LearningPath> learningPaths = new ArrayList<>();
+
+        for (String preference : preferences) {
+            learningPaths.add(new LearningPath(preference));
+        }
+
+        return learningPaths;
+    }
+
+    public List<Activity> recommendNextActivity(Student student, LearningPath lp) {
+        return preferenceManager.recommendNextActivity(student, lp);
+    }
 }
